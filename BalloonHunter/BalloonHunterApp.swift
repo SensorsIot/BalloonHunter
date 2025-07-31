5d7dc8d8316dc87bc6626feb04fe7a28e37be786
Andreas Spiess <sensorsiot@gmail.com>
2025-07-31 11:45:12 +0200

    Initial Commit

---
+++ BalloonHunter/BalloonHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonHunterApp.swift
+//  BalloonHunter
+//
+//  Created by Andreas Spiess on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BalloonHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalloonHunter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BalloonHunter
+//
+//  Created by Andreas Spiess on 31.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
